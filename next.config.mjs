47fcfe260843b18dd575639921afdb5c3a4902ee
Sciabarrasi <gianluca_sciabarrasi@hotmail.com>
2025-06-19 15:43:19 -0300

    fix(next-auth, next.config): enable AWS Amplify integration

--- next.config.mjs
+++ next.config.mjs
@@ -7,8 +7,34 @@ const nextConfig = {
   images: {
     domains: [
       'hebbkx1anhila5yf.public.blob.vercel-storage.com',
-      'res.cloudinary.com'
+      'res.cloudinary.com',
+      'master.d12fg2zw920c0m.amplifyapp.com',
+      'echiozzi.com',
+      'www.echiozzi.com',
+      'localhost'
     ],
+  },
+
+  experimental: {
+    serverComponentsExternalPackages: ["@aws-sdk"],
+  },
+
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff'
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'DENY'
+          }
+        ]
+      }
+    ]
   }
 };
 
